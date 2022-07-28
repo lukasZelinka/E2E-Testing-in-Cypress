e57ccc62eececb8c318375ed6c2a44870d1287e1
lukasZelinka <77839958+lukasZelinka@users.noreply.github.com>
2022-07-28 11:59:32 +0200

    simplification of names in the it() function and better code structure

--- cypress/e2e/dashboard_spec.cy.ts
+++ cypress/e2e/dashboard_spec.cy.ts
@@ -3,47 +3,36 @@ describe('dashboard page', () => {
     cy.visit("/dashboard");
   });
 
- it("greets with page", () => {
+  it("greets with page", () => {
     cy.contains("h2", "Top Heroes");
   });
 
-  it("invalid Hero with no match", () => {
+  it("Hero not found", () => {
     cy.get("[id='search-box']").type("invalidHero{enter}");
     cy.contains(' HeroService: no heroes matching "invalidHero" ');
   });
 
-   it("valid hero search", () => {
-    cy.get("[id='search-box']").type("Bombasto{enter}");
-    cy.contains("[class='search-result'] li a", 'Bombasto').click();
-    cy.url().should("eq", "http://localhost:4200/detail/13");
-  });
-
-  it("showing report messages", () => {
+  it("Hero found", () => {
     cy.get("[id='search-box']").type("Bombasto{enter}");
     cy.contains("app-messages", ' HeroService: found heroes matching "Bombasto" ');
-  });
-
-
-   it("disappearance of report messages ", () => {
-    cy.contains("h2", "Messages");
-    cy.get('app-messages').should('have.length', 1).find('.clear').click();
-    cy.get('app-messages').should('not.be.visible')
-    });
-
- it("visit Heroe's detail ", () => {
-    cy.contains("Bombasto").should("have.attr", "href", "/detail/13");
-    cy.contains("[class='heroes-menu']  a", 'Bombasto').click();
+    cy.contains("[class='search-result'] li a", 'Bombasto').click();
+    cy.url().should("eq", "http://localhost:4200/detail/13");
     cy.contains("[type='button']", "go back").click();
   });
 
+  it("edit Hero", () => {
+     cy.contains("[class='heroes-menu']  a", 'Bombasto').click();
+     cy.url().should("eq", "http://localhost:4200/detail/13");
+     cy.get("[id='hero-name']").type(' Surname');
+     cy.contains("[type='button']", "save").click();
+     cy.url().should("eq", "http://localhost:4200/dashboard");
+     cy.contains("[class='heroes-menu'] a ", "Bombasto Surname");
+   });
 
-  it("changing the name of the Hero", () => {
-    cy.contains("[class='heroes-menu']  a", 'Bombasto').click();
-    cy.url().should("eq", "http://localhost:4200/detail/13");
-    cy.get("[id='hero-name']").type(' Surname');
-    cy.contains("[type='button']", "save").click();
-    cy.url().should("eq", "http://localhost:4200/dashboard");
-    cy.contains("[class='heroes-menu'] a ", "Bombasto Surname");
+  it("clear messages ", () => {
+    cy.contains("h2", "Messages");
+    cy.get('app-messages').should('have.length', 1).find('.clear').click();
+    cy.get('app-messages').should('not.be.visible')
   });
-  
+ 
 })
--- cypress/e2e/heroes_spec.cy.ts
+++ cypress/e2e/heroes_spec.cy.ts
@@ -7,44 +7,46 @@ describe('heroes page', () => {
     cy.contains("h2", "My Heroes");
   });
 
-  it("adding a new hero to the list ", () => {
+  it("Heroe's detail", () => {
+    cy.get("[class='heroes'] li:first a ",).click();
+    cy.url().should("eq", "http://localhost:4200/detail/12");
+    cy.contains("[type='button']", "go back").click();
+  });
+
+  it("add Hero", () => {
    cy.get("[id='new-hero']").type("Batman");  
    cy.get("[class='add-button']").click();
    cy.contains("li", "Batman");
   });
-  
-  it("removing the hero from the list", () => {
+
+  it("edit Hero", () => {
+   cy.contains("[class='heroes'] li:first a ", "Dr. Nice").click();
+   cy.url().should("eq", "http://localhost:4200/detail/12");
+   cy.get("[id='hero-name']").type(' Surname');
+   cy.contains("[type='button']", "save").click();
+   cy.url().should("eq", "http://localhost:4200/heroes");
+   cy.contains("[class='heroes'] li:first a ", "Dr. Nice Surname")
+  });
+
+  it("delete Hero", () => {
    cy.get("[class='heroes'] li:first button").click().should('not.exist');
   });
 
-  it("showing report messages", () => {
-   cy.get("[id='new-hero']").type("Batman");  
-   cy.get("[class='add-button']").click();
-   cy.contains("app-messages", 'HeroService: added hero w/ id=');
+  it("show message", () => {
+    cy.get("[id='new-hero']").type("Batman");  
+    cy.get("[class='add-button']").click();
+    cy.contains("app-messages", 'HeroService: added hero w/ id=');
   });
-
-  it("disappearance of report messages ", () => {
-   cy.contains("h2", "Messages");
-   cy.get('app-messages').should('have.length', 1).find('.clear').click();
-   cy.get('app-messages').should('not.be.visible')
-  });
-
-  it("visit Heroe's detail ", () => {
-    cy.get("[class='heroes'] li:first a ",).click();
-     cy.url().should("eq", "http://localhost:4200/detail/12");
-     cy.contains("[type='button']", "go back").click();
-  });
-
-   it("changing the name of the Hero", () => {
-    cy.contains("[class='heroes'] li:first a ", "Dr. Nice").click();
-    cy.url().should("eq", "http://localhost:4200/detail/12");
-    cy.get("[id='hero-name']").type(' Surname');
-    cy.contains("[type='button']", "save").click();
-    cy.url().should("eq", "http://localhost:4200/heroes");
-    cy.contains("[class='heroes'] li:first a ", "Dr. Nice Surname")
+  
+  it("clear messages ", () => {
+    cy.contains("h2", "Messages");
+    cy.get('app-messages').should('have.length', 1).find('.clear').click();
+    cy.get('app-messages').should('not.be.visible')
   });
 
 });
+
+
 
 
 
